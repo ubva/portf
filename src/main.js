4111ed0890e1c957d223054044cf8b2b5bf81bb3
ubva <aubaydullayevaa4@gmail.com>
2024-01-11 11:57:17 +0500

    add new

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from "pinia";
 import App from './App.vue'
 import router from "./routes/router";
 import './assets/sass/main.scss';
-// import "./stores/style"
 
 const app =createApp(App)
 app.use(router)
